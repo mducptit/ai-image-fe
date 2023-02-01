f901ca09a003e71c658bc91c271e146d67784e7f
ducvm3 <ducvm3@vng.com.vn>
2023-02-01 22:51:45 +0700

    feat: init source code

---
+++ src/components/Loader.jsx
@@ -0,0 +1,6 @@
+import { Spin } from 'antd'
+import React from 'react'
+
+const Loader = () => <Spin tip="Loading..."></Spin>
+
+export default Loader
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import CardCustom from './Card'
+import FormField from './FormField'
+import Loader from './Loader'
+
+export { CardCustom, FormField, Loader }
